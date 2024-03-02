644f370b8684b1400c727fb29a3e82c1dd99ea84
maritocuate <mario300@gmail.com>
2024-03-02 12:38:31 -0300

    chore: leva controls

--- src/components/Platform.tsx
+++ src/components/Platform.tsx
@@ -1,21 +1,30 @@
 import { Cylinder, SpotLight, useGLTF } from '@react-three/drei'
+import { useControls } from 'leva'
 
 export default function Platform() {
   const { scene } = useGLTF('/models/round_platform/scene.gltf')
+
+  const { position } = useControls('Spotlight', {
+    position: {
+      value: [-40, 10, -70],
+      step: 1,
+    },
+  })
 
   return (
     <>
       <mesh>
         <primitive
           object={scene}
-          scale={[8, 8, 8]}
+          scale={8}
           position={[-40, -10, -70]}
         ></primitive>
-        <Cylinder scale={[19, 39, 19]} position={[-40, 10, -70]}>
+        {/* <Cylinder scale={[19, 39, 19]} position={[-40, 10, -70]}>
           <meshBasicMaterial transparent opacity={0.3} color="blue" />
-        </Cylinder>
+        </Cylinder> */}
+
         <SpotLight
-          position={[-10, 50, -17]}
+          position={position}
           distance={15}
           angle={0.15}
           attenuation={5}
